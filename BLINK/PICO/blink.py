1f604d46eecc3218e49e7fea3344b6caaaeb521e
Gloria <80504814+GloRLuna@users.noreply.github.com>
2023-01-16 00:11:34 -0600

    Update blink.py

--- BLINK/PICO/blink.py
+++ BLINK/PICO/blink.py
@@ -1,8 +1,9 @@
-from machine import Pin, Timer
-led = Pin(25, Pin.OUT)
-timer = Timer()
+import time
+from machine import Pin
+led=Pin(13,Pin.OUT)        #create LED object from pin13,Set Pin13 to output
 
-def blink(timer):
-    led.toggle()
-
-timer.init(freq=2.5, mode=Timer.PERIODIC, callback=blink)
+while True:
+  led.value(1)            #Set led turn on
+  time.sleep(0.75)
+  led.value(0)            #Set led turn off
+  time.sleep(0.75)
